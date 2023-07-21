f3db924ef47b7cc4b94f6e100a5de8a469d5e40e
al1yew <vasifja@code.edu.az>
2023-07-22 00:40:16 +0400

    finishing header

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,50 +1,70 @@
-/*
+const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+).matches;
+const storedDarkMode = localStorage.getItem("dark_theme") === "true";
+const isDarkTheme = storedDarkMode || prefersDarkMode;
 
-import { createContext, useContext, useState, useEffect } from "react";
+function updateDarkModeUI(isDark) {
+    if (isDark) {
+        $("body").addClass("darkmode");
+        $(".moon").hide();
+        $(".sun").show();
+        localStorage.setItem("dark_theme", true);
+    } else {
+        $("body").removeClass("darkmode");
+        $(".sun").hide();
+        $(".moon").show();
+        localStorage.setItem("dark_theme", false);
+    }
+}
 
-const ThemeContext = createContext();
-
-const getInitialDarkMode = () => {
-    const prefersDarkMode = window.matchMedia(
-        "(prefers-color-scheme:dark)"
-    ).matches;
-
-    const storedDarkMode = localStorage.getItem("dark_theme") === "true";
-
-    return storedDarkMode || prefersDarkMode;
-};
-
-export const ThemeProvider = ({ children }) => {
-    const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
-
-    const toggleDarkTheme = () => {
-        const newDarkTheme = !isDarkTheme;
-        setIsDarkTheme(newDarkTheme);
-        localStorage.setItem("dark_theme", newDarkTheme);
-    };
-
-    useEffect(() => {
-        document.body.classList.toggle("dark_theme", isDarkTheme);
-    }, [isDarkTheme]);
-
-    return (
-        <ThemeContext.Provider
-            value={{
-                isDarkTheme,
-                toggleDarkTheme,
-            }}
-        >
-            {children}
-        </ThemeContext.Provider>
-    );
-};
-
-export const useThemeContext = () => useContext(ThemeContext);
-
-*/
+updateDarkModeUI(isDarkTheme);
 
 $(function () {
-    $(".darkmodebtn").on("click", function () {
-        $("body").toggleClass("darkmode");
+    $(".preloader").addClass("d-none");
+
+    //#region dark mode and light mode
+
+    $(document).on("click", ".modechanger", function () {
+        let newTheme;
+        if ($(this).hasClass("sun")) {
+            newTheme = false;
+        } else {
+            newTheme = true;
+        }
+        localStorage.setItem("dark_theme", newTheme);
+        updateDarkModeUI(newTheme);
     });
+
+    //#endregion dark mode and light mode
+
+    //#region headersearch
+
+    $(document).on("click", function (e) {
+        const isParentClass = $(e.target).parents(".search").length > 0;
+
+        if (isParentClass) {
+            $(".searchmenu").fadeIn(150);
+            $(".searchicon ion-icon").attr("name", "close-outline");
+            $(".searchicon").addClass("closesearchheader");
+        } else if (!isParentClass || $(".searchinput").val() == "") {
+            $(".searchmenu").fadeOut(150);
+            $(".searchinput").val("");
+
+            $(".searchicon ion-icon").attr("name", "search-outline");
+            $(".searchicon").removeClass("closesearchheader");
+        }
+
+        console.log("salam");
+    });
+
+    $(document).on("click", ".closesearchheader", function () {
+        console.log("salam");
+        $(".closesearchheader ion-icon").attr("name", "search-outline");
+        $(".searchinput").val("");
+        $(".searchmenu").fadeOut(150);
+        $(this).removeClass("closesearchheader");
+    });
+
+    //#endregion headersearch
 });
